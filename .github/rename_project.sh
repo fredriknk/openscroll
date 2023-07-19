700be6c949a2e58d65dcba79a8d17304dd74b740
fredriknk <fredriknk@users.noreply.github.com>
2023-07-19 19:28:49 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="fredriknk"
+original_name="openscroll"
+original_urlname="openscroll"
+original_description="Awesome openscroll created by fredriknk"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv openscroll $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
